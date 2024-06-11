5a3e1db8c329e349b12bb8189aa2f0deda17c403
Cloudyx <durjs96@gmail.com>
2024-06-11 20:30:05 +0900

    Update

--- This Is Coding Test with Python/C++/ThisIsCodingTest-Basic/Greedy/[17-2] 38.정확한 순위.cpp
+++ This Is Coding Test with Python/C++/ThisIsCodingTest-Basic/Greedy/[17-2] 38.정확한 순위.cpp
@@ -2,22 +2,33 @@
 
 #include "core.h"
 
-#define CP_NUM "X-Y"
+#define CP_NUM "17-2"
 
-#ifdef PX_Y
+#ifdef P17_2
 #ifdef VSTOOL
 
 #include <iostream>
+#include <vector>
+#include <array>
 #include "CodingTester.h"
 
 using namespace std;
 
 struct Param {
-	int n{};
+	int n{}, m{};
+	vector<array<int, 2>> v{};
 
 	friend istream& operator>>( istream& is, Param& self )
 	{
-		is >> self.n;
+		is >> self.n >> self.m;
+
+		int tmp1{}, tmp2{};
+
+		for ( int i{}; i < self.m; ++i )
+		{
+			is >> tmp1 >> tmp2;
+			self.v.push_back( {tmp1, tmp2} );
+		}
 
 		return is;
 	}
@@ -68,8 +79,13 @@ struct std::formatter<TestSet> {
 		auto out = format_to( ctx.out(), "{:^6}| ", strnum );
 
 		// Parameter Line
-		out = format_to( out, "n: {}", ts.param.n );
+		out = format_to( out, "n: {} | m: {}", ts.param.n, ts.param.m );
 		out = format_to( out, "\n{:^6}| ", "" );
+
+		for ( int i{}; i < ts.param.m; ++i )
+		{
+			out = format_to( out, "\n{:^6}| {} -> {}", "", ts.param.v[i][0], ts.param.v[i][1]);
+		}
 
 		// Result Line
 		out = format_to( out, "\n{:^6}| ", "" );
@@ -86,7 +102,7 @@ int main()
 {
 	cout << "Practice " << CP_NUM << " =======================" << endl;
 
-	auto test_sets{ ReadTestFile<TestSet>( "../../../TestSets/"+ string( CP_NUM ) + ".txt" ) };
+	auto test_sets{ ReadTestFile<TestSet>( "../../../TestSets/" + string( CP_NUM ) + ".txt" ) };
 
 	cout << "My Solution =========================\n";
 	for ( int i{}; const auto & test_set : test_sets ) {
@@ -109,6 +125,29 @@ int main()
 Result MySolution( Param param )
 {
 	Result result{};
+
+	vector<vector<int>> dt{};
+
+	for ( int i{}; i < param.m; ++i )
+	{
+		dt.push_back( {} );
+		for ( int j{}; j < param.m; ++j )
+		{
+			dt.back().push_back( param.n );
+		}
+	}
+
+	/*
+	   1   2   3   4   5   6
+	1  0               1	 
+	2      0 	  -1 	 
+	3          0   1 	 	 
+	4      1  -1   0	 	 
+	5 -1               0	 
+	6                      0
+	
+	*/
+
 	return result;
 }
 
